252b37fc616180f17e58c560df71f2180ee7b339
doanduchieupw <doanduchieu.pw12@gmail.com>
2022-05-10 10:15:50 +0700

    build df layout

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,12 @@
 import { Home, Following, Profile, Upload, Search } from '~/pages';
-import { OnlyHeader } from '~/Components/Layout';
+import { HeaderOnly } from '~/Components/Layout';
 
 //Public Routes
 const publicRoutes = [
     { path: '/', component: Home },
     { path: '/following', component: Following },
     { path: '/profile', component: Profile },
-    { path: '/upload', component: Upload, layout: OnlyHeader },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
     { path: '/search', component: Search, layout: null },
 ];
 const privateRoutes = [];
